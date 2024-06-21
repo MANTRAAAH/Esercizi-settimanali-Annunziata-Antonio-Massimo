7a8e78f9b3b7a00dca852e48a06611ee140c9154
Annunziata Antonio Massimo <ant.annunz.02@gmail.com>
2024-06-21 09:20:56 +0200

    start

---
+++ 21 Giugno 2024/Esercizio S1 Back End/Esercizio S1 Back End/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Esercizio_S1_Back_End
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
